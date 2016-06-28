3dcf6c0b7f69e0034e03c31a11e03455568fe47d
RomanTabi <roman.tabi@gmail.com>
2016-06-28 11:14:41 +0200

    Database, model

--- TreeFilter/db/seeds.rb
+++ TreeFilter/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Item.delete_all
+Item.create(parentId: 1 , myId: 1, text: 'Prislusenstvo')
+Item.create(parentId: 2 , myId: 2, text: 'Rukavice')
+Item.create(parentId: 3 , myId: 3, text: 'Oblecenie')
+Item.create(parentId: 3 , myId: 4, text: 'Nohavice')
+Item.create(parentId: 3 , myId: 5, text: 'Tricka')
+Item.create(parentId: 6 , myId: 6, text: 'Topanky')
+Item.create(parentId: 6 , myId: 7, text: 'Tenisky')
+Item.create(parentId: 6 , myId: 8, text: 'Papuce')
